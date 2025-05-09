ab2f1816678a881d3d7872284a68ad1d1ceb7deb
Habibi-Pro <devartest1@gmail.com>
2025-05-09 15:44:07 +0430

    Update settings.py

--- ready_project/settings.py
+++ ready_project/settings.py
@@ -29,6 +29,8 @@ TEMPLATES = [{
             'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
+            'DIRS': [BASE_DIR / 'main' / 'templates'],
+
         ],
     },
 }]
